ab00c3a5e1ca5dd2c1744530209fce3679e89149
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2023-03-16 09:47:31 -0400

    projsplx normalized coding style, added comments (paper ref, link to ALStructure)

--- R/projsplx.R
+++ R/projsplx.R
@@ -1,23 +1,31 @@
+## adapted from ALStructure at https://github.com/StoreyLab/alstructure/blob/master/R/factor.R
 
-projsplx<-function(y)
-{
-	m = length(y)
-	bget = FALSE
-	s <- sort(y, decreasing = TRUE)
-	tmpsum = 0
+## Project a vector onto the simplex
+##
+## @param y Length-`n` vector.
+##
+## @return Length-`n` vector which is the projection of `y` onto the `n`-dimensional simplex.
+##
+## @references
+## Chen, Y., and X. Ye. 2011. “Projection Onto A Simplex.” ArXiv E-Prints, January.
+projsplx <- function(y) {
+    m <- length(y)
+    bget <- FALSE
+    s <- sort(y, decreasing = TRUE)
+    tmpsum <- 0
 
-	for (ii in 1:(m - 1)){
-		tmpsum = tmpsum + s[ii]
-	        tmax = (tmpsum - 1) / ii
-		if (tmax >= s[ii + 1]){
-			bget = TRUE
-		        break
-		}
-	}
-
-	if (!bget){
-		tmax = (tmpsum + s[m] - 1)/m
-	}
-
-	x <- pmax(y - tmax, rep(0, m));
+    for (ii in 1:(m - 1)){
+        tmpsum <- tmpsum + s[ii]
+        tmax <- (tmpsum - 1) / ii
+        if (tmax >= s[ii + 1]) {
+            bget <- TRUE
+            break
+        }
+    }
+    
+    if (!bget)
+        tmax <- (tmpsum + s[m] - 1)/m
+    
+    x <- pmax(y - tmax, rep(0, m))
+    return( x )
 }
