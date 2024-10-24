61caabda6f0201b8ac9377cd6a260c97140dd60a
amikasood <amikasood@gmail.com>
2024-10-24 15:06:43 -0400

    added Q stretch to admixcor

--- R/admixcor.R
+++ R/admixcor.R
@@ -12,7 +12,9 @@ admixcor <- function(
                      tol = sqrt( .Machine$double.eps ), # 1e-15
                      nstep_max = 100000,
                      report_freq = 1000,
-                     fix_L = FALSE
+                     fix_L = FALSE,
+		     stretch = FALSE,
+		     tol_stretch = -0.01
                      ) {
     # process options
     Q_type <- match.arg( Q_type )
@@ -62,10 +64,19 @@ admixcor <- function(
             break
         
         # apply the updates, one at the time
-        R1 <- update_R( ThetaSR, Q0, L0 )
-        L1 <- update_L( ThetaSR, Q0, R1, gamma, algorithm = L_algorithm, fix_L = fix_L )
-        Q1 <- update_Q( ThetaSR, L1, R1, delta, I, algorithm = Q_algorithm, vertex_refine = vertex_refine )
-        
+	if ( stretch ) {
+		Q1 <- update_Q( ThetaSR, L0, R0, delta, I, algorithm = Q_algorithm, vertex_refine = vertex_refine )
+		Q1 <- stretch_Q( Q1, tol = tol_stretch )$Q
+		Q1[Q1 < 0] <- 0
+		Q1 <- Q1 / rowSums( Q1 )
+		L1 <- update_L( ThetaSR, Q1, R0, gamma, algorithm = L_algorithm, fix_L = fix_L )
+		R1 <- update_R( ThetaSR, Q1, L1 )
+	}
+	else {
+		R1 <- update_R( ThetaSR, Q0, L0 )
+	        L1 <- update_L( ThetaSR, Q0, R1, gamma, algorithm = L_algorithm, fix_L = fix_L )
+		Q1 <- update_Q( ThetaSR, L1, R1, delta, I, algorithm = Q_algorithm, vertex_refine = vertex_refine )
+	}
         # calculate step sizes, to assess convergence
 	ndQ <- norm( Q0 - Q1, "F" )^2
         
